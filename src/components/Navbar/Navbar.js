c9b991cba1bfe4307a901cf0c68990ce50ccbe47
MeryAmun <chebesiawah@gmail.com>
2023-03-15 03:39:31 +0100

    navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { logo } from '../../assets';
 import { Link } from 'react-router-dom';
 import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import CloseIcon from '@mui/icons-material/Close';
 import './navbar.css'
+import { NavSmall } from './styles';
+import Twitter from '@mui/icons-material/Twitter';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
 
     var navbar = document.querySelector('nav')
 
@@ -51,7 +59,51 @@ window.onscroll = function() {
                 <Link to='/start-a-project'>Book A Call</Link>
                 </button>
             </div>
+             <MenuOutlinedIcon fontSize='large' className="navbar__linksMenuIcon" onClick={() => setOpen(true)}/>
+
         </div>
+
+        {/* ===================FOR SMALL SCREENS   ================================ */}
+
+       <NavSmall show={open} onMouseLeave={() => setOpen(false)}>
+       {/* <MenuOutlinedIcon fontSize='large' className="navbar__linkMenuIconSmall"/> */}
+       <CloseIcon fontSize='large' className="navbar__linkMenuIconSmall" onClick={() => setOpen(false)}/>
+       <div className="navbar__linksSmall">
+            <div className="navbar__linkSmall">
+     <Link to='/work'>Work</Link>
+            </div>
+            <div className="navbar__link navbar__linkBoxSmall">
+     <Link to='/services'>Services</Link>
+     <KeyboardArrowDownOutlinedIcon fontSize='small' className='navbar__linkIconSmall'/>
+            </div>
+            <div className="navbar__linkSmall">
+     <Link to='/pricing'>Pricing</Link>
+            </div>
+            <div className="navbar__linkSmall">
+     <Link to='/about-us'>About Us</Link>
+            </div>
+            <div className="navbar__linkSmall">
+     <Link to='/careers'>Careers</Link>
+            </div>
+            <div className="navbar__link navbar__linkBoxSmall">
+     <Link to='/tech'>Tech</Link>
+     <KeyboardArrowDownOutlinedIcon fontSize='small' className='navbar__linkIconSmall'/>
+            </div>
+            <div className="navbar__linkSmall">
+     <Link to='/blog'>Blog</Link>
+            </div>
+            <div className="navbar__linkBookingSmall">
+                <button>
+                <Link to='/start-a-project'>Book A Call</Link>
+                </button>
+            </div>
+            </div>
+            <div className="navbar__navbarSmallSocial">
+<TwitterIcon className='navbar__socialIcon'/>
+<FacebookIcon className='navbar__socialIcon'/>
+<LinkedInIcon className='navbar__socialIcon'/>
+            </div>
+       </NavSmall>
     </div>
   )
 }
---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const NavSmall = styled.div`
+position: fixed;
+top: 0;
+bottom: 0;
+right: 0;
+background: #0044FF;
+display:flex;
+flex-direction:column;
+justify-content:space-between;
+width: 300px;
+z-index: 16;
+list-style: none;
+padding: 20px;
+text-align: start;
+transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+transition: transform 0.2s;
+  }
+  `
