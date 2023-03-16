cfde9b29e7aed2f91e939da17ce1a10334c4ba7f
MeryAmun <chebesiawah@gmail.com>
2023-03-16 18:32:37 +0100

    video control

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,114 +1,129 @@
-import React, {useState} from 'react';
-import { logo } from '../../assets';
-import { Link } from 'react-router-dom';
-import {HiOutlineMenuAlt2} from 'react-icons/hi'
-import {FaFacebookF, FaTwitter} from 'react-icons/fa'
-import {TiSocialLinkedin} from 'react-icons/ti'
-import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+import React, { useState } from "react";
+import { logo } from "../../assets";
+import { Link } from "react-router-dom";
+import { HiOutlineMenuAlt2 } from "react-icons/hi";
+import { FaFacebookF, FaTwitter } from "react-icons/fa";
+import { TiSocialLinkedin } from "react-icons/ti";
+import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
 
-import './navbar.css'
-import { NavSmall } from './styles';
-
+import "./navbar.css";
+import { NavSmall } from "./styles";
 
 const Navbar = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    var navbar = document.querySelector('.navbar')
-    var navbarLink = document.querySelector('.navbar__link')
+  var navbar = document.querySelector(".navbar");
+  var navbarLink = document.querySelector(".navbar__link");
 
-window.onscroll = function() {
-
-  // pageYOffset or scrollY
-  if (window.pageYOffset > 10) {
-    navbar.classList.add('scrolled');
-    navbarLink.classList.add('scrolled')
-  } else {
-    navbar.classList.remove('scrolled')
-    navbarLink.classList.remove('scrolled')
-  }
-}
+  window.onscroll = function () {
+    // pageYOffset or scrollY
+    if (window.pageYOffset > 10) {
+      navbar.classList.add("scrolled");
+      navbarLink.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+      navbarLink.classList.remove("scrolled");
+    }
+  };
   return (
-    <div className='navbar nav-colored'>
-        <Link to="/">
-            <img src={logo} alt="" className='navbar__logo'/>
-        </Link>
-        <div className="navbar__links">
-            <div className="navbar__link">
-     <Link to='/work'>Work</Link>
-            </div>
-            <div className="navbar__link navbar__linkBox">
-     <Link to='/services'>Services</Link>
-     <KeyboardArrowDownOutlinedIcon fontSize='small' className='navbar__linkIcon'/>
-            </div>
-            <div className="navbar__link">
-     <Link to='/pricing'>Pricing</Link>
-            </div>
-            <div className="navbar__link">
-     <Link to='/about-us'>About Us</Link>
-            </div>
-            <div className="navbar__link">
-     <Link to='/careers'>Careers</Link>
-            </div>
-            <div className="navbar__link navbar__linkBox">
-     <Link to='/tech'>Tech</Link>
-     <KeyboardArrowDownOutlinedIcon fontSize='small' className='navbar__linkIcon'/>
-            </div>
-            <div className="navbar__link">
-     <Link to='/blog'>Blog</Link>
-            </div>
-            <div className="navbar__linkBooking">
-                <button>
-                <Link to='/start-a-project'>BOOK A CALL</Link>
-                </button>
-            </div>
-             <HiOutlineMenuAlt2  className="navbar__linksMenuIcon" onClick={() => setOpen(true)}/>
-
+    <div className="navbar nav-colored">
+      <Link to="/">
+        <img src={logo} alt="" className="navbar__logo" />
+      </Link>
+      <div className="navbar__links">
+        <div className="navbar__link">
+          <Link to="/work">Work</Link>
         </div>
+        <div className="navbar__link navbar__linkBox">
+          <Link to="/services">Services</Link>
+          <KeyboardArrowDownOutlinedIcon
+            fontSize="small"
+            className="navbar__linkIcon"
+          />
+        </div>
+        <div className="navbar__link">
+          <Link to="/pricing">Pricing</Link>
+        </div>
+        <div className="navbar__link">
+          <Link to="/about-us">About Us</Link>
+        </div>
+        <div className="navbar__link">
+          <Link to="/careers">Careers</Link>
+        </div>
+        <div className="navbar__link navbar__linkBox">
+          <Link to="/tech">Tech</Link>
+          <KeyboardArrowDownOutlinedIcon
+            fontSize="small"
+            className="navbar__linkIcon"
+          />
+        </div>
+        <div className="navbar__link">
+          <Link to="/blog">Blog</Link>
+        </div>
+        <div className="navbar__linkBooking">
+          <button>
+            <Link to="/start-a-project">BOOK A CALL</Link>
+          </button>
+        </div>
+        <HiOutlineMenuAlt2
+          className="navbar__linksMenuIcon"
+          onClick={() => setOpen(true)}
+        />
+      </div>
 
-        {/* ===================FOR SMALL SCREENS   ================================ */}
+      {/* ===================FOR SMALL SCREENS   ================================ */}
 
-       <NavSmall show={open} onMouseLeave={() => setOpen(false)}>
- 
-       <HiOutlineMenuAlt2 fontSize='large' className="navbar__linkMenuIconSmall" onClick={() => setOpen(false)}/>
-       {/* <AiOutlineClose fontSize='large' className="navbar__linkMenuIconSmall" onClick={() => setOpen(false)}/> */}
-       <div className="navbar__linksSmall">
-            <div className="navbar__linkSmall">
-     <Link to='/work'>Work</Link>
-            </div>
-            <div className="navbar__linkSmall navbar__linkBoxSmall">
-     <Link to='/services'>Services</Link>
-     <KeyboardArrowDownOutlinedIcon fontSize='large' className='navbar__linkIconSmall'/>
-            </div>
-            <div className="navbar__linkSmall">
-     <Link to='/pricing'>Pricing</Link>
-            </div>
-            <div className="navbar__linkSmall">
-     <Link to='/about-us'>About Us</Link>
-            </div>
-            <div className="navbar__linkSmall">
-     <Link to='/careers'>Careers</Link>
-            </div>
-            <div className="navbar__linkSmall navbar__linkBoxSmall">
-     <Link to='/tech'>Tech</Link>
-     <KeyboardArrowDownOutlinedIcon fontSize='large' className='navbar__linkIconSmall'/>
-            </div>
-            <div className="navbar__linkSmall">
-     <Link to='/blog'>Blog</Link>
-            </div>
-            <div className="navbar__linkBookingSmall">
-                <button>
-                <Link to='/start-a-project'>BOOK A CALL</Link>
-                </button>
-            </div>
-            </div>
-            <div className="navbar__navbarSmallSocial">
-<FaTwitter className='navbar__socialIcon'/>
-<FaFacebookF className='navbar__socialIcon'/>
-<TiSocialLinkedin className='navbar__socialIcon'/>
-            </div>
-       </NavSmall>
+      <NavSmall show={open} onMouseLeave={() => setOpen(false)}>
+        <HiOutlineMenuAlt2
+          fontSize="large"
+          className="navbar__linkMenuIconSmall"
+          onClick={() => setOpen(false)}
+        />
+        {/* <AiOutlineClose fontSize='large' className="navbar__linkMenuIconSmall" onClick={() => setOpen(false)}/> */}
+        <div className="navbar__linksSmall">
+          <div className="navbar__linkSmall">
+            <Link to="/work">Work</Link>
+          </div>
+          <div className="navbar__linkSmall navbar__linkBoxSmall">
+            <Link to="/services">Services</Link>
+            <KeyboardArrowDownOutlinedIcon
+              fontSize="large"
+              className="navbar__linkIconSmall"
+            />
+          </div>
+          <div className="navbar__linkSmall">
+            <Link to="/pricing">Pricing</Link>
+          </div>
+          <div className="navbar__linkSmall">
+            <Link to="/about-us">About Us</Link>
+          </div>
+          <div className="navbar__linkSmall">
+            <Link to="/careers">Careers</Link>
+          </div>
+          <div className="navbar__linkSmall navbar__linkBoxSmall">
+            <Link to="/tech">Tech</Link>
+            <KeyboardArrowDownOutlinedIcon
+              fontSize="large"
+              className="navbar__linkIconSmall"
+            />
+          </div>
+          <div className="navbar__linkSmall">
+            <Link to="/blog">Blog</Link>
+          </div>
+          <div className="navbar__linkBookingSmall">
+            <button>
+              <Link to="/start-a-project">BOOK A CALL</Link>
+            </button>
+          </div>
+        </div>
+        <div className="navbar__navbarSmallSocial">
+          <FaTwitter className="navbar__socialIcon" />
+          <FaFacebookF className="navbar__socialIcon" />
+          <TiSocialLinkedin className="navbar__socialIcon" />
+        </div>
+      </NavSmall>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,70 +1,100 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import { hero, review, reviewLogo } from '../../assets';
-import { FaQuoteLeft } from 'react-icons/fa'
-import Slide from 'react-reveal/Slide';
-import './home.css'
-import { serviceOptions } from '../../staticData/staticData';
-import { ServiceOptionCard } from '../../cards';
-import { AiFillPlayCircle } from 'react-icons/ai'
+import React, { useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import { hero, review, reviewLogo } from "../../assets";
+import { FaQuoteLeft } from "react-icons/fa";
+import Slide from "react-reveal/Slide";
+import "./home.css";
+import { serviceOptions } from "../../staticData/staticData";
+import { ServiceOptionCard } from "../../cards";
+import { AiFillPlayCircle } from "react-icons/ai";
 
 const Home = () => {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
   return (
-    <div className='home'>
-        <div className="home__hero">
-           <div className='home__heroVideo'>
-           <video src={hero} autoPlay muted loop ></video>
-           </div>
-            <div className="home__header">
-                <Slide bottom>
-                <h1>Custom Product & <br /> Software Development <br /> Focused On Your Success</h1>
-                </Slide>
-                  <Slide bottom>
-                <button className="home__headerButton">
-                <Link to='/start-a-project'>BOOK A CALL</Link>
-                </button>
-                </Slide>
-            </div>
+    <div className="home">
+      <div className="home__hero">
+        <div className="home__heroVideo">
+          <video src={hero} autoPlay muted loop></video>
         </div>
-        <div className="home__services">
-        <Slide bottom>
-          <div className="home__servicesHeader">
-            <h2 className="home__servicesHeaderTitle">
-           Services
-            </h2>
-            <p className="home__servicesHeaderText">
-            We can help you bring your product to life - <br /> whether it's a Minimum Viable Product, UX/UI <br /> Services or help to scale your company.
-            </p>
-          </div>
+        <div className="home__header">
+          <Slide bottom>
+            <h1>
+              Custom Product & <br /> Software Development <br /> Focused On
+              Your Success
+            </h1>
           </Slide>
           <Slide bottom>
-          <div className="home__serviceOptions">
-           {
-            serviceOptions?.map(({icon,title,text},index) => (
-              <ServiceOptionCard key={index} title={title} icon={icon} text={text}/>
-            ))
-           }
-          </div>
+            <button className="home__headerButton">
+              <Link to="/start-a-project">BOOK A CALL</Link>
+            </button>
           </Slide>
         </div>
-        <div className="home__review">
-          <div className="home__reviewVideo">
-{/* <video src='https://www.youtube.com/watch?v=ib43NVgp_x0'></video> */}
-<video src={review}></video>
-<AiFillPlayCircle  className='home__reviewVideoIcon'/>
-{/* <span className='home__reviewVideoIcon'><AiFillPlayCircle /></span> */}
+      </div>
+      <div className="home__services">
+        <Slide bottom>
+          <div className="home__servicesHeader">
+            <h2 className="home__servicesHeaderTitle">Services</h2>
+            <p className="home__servicesHeaderText">
+              We can help you bring your product to life - <br /> whether it's a
+              Minimum Viable Product, UX/UI <br /> Services or help to scale
+              your company.
+            </p>
           </div>
-         <Slide bottom>
-         <div className="home__reviewMessage">
-            <FaQuoteLeft color='#0044FF' className='home__reviewQuote'/>
-            <p className="home__reviewText">We are very happy to work with Altar.io. The collaboration goes far beyond a standard IT service: with Altar.io we have a sparring partner that challenges us in our product decisions and is truly passionate about the results developed. The level of professionalism and dedication is exceptional in this industry and we are eager to strengthen our collaboration in the future.</p>
-            <span className="home__reviewer">Philip Schoch - Apiax, Co-Founder</span>
-            <img src={reviewLogo} alt="" className='home__reviewClient'/>
+        </Slide>
+        <Slide bottom>
+          <div className="home__serviceOptions">
+            {serviceOptions?.map(({ icon, title, text }, index) => (
+              <ServiceOptionCard
+                key={index}
+                title={title}
+                icon={icon}
+                text={text}
+              />
+            ))}
           </div>
-         </Slide>
+        </Slide>
+      </div>
+      <div className="home__review">
+        <div className="home__reviewVideo">
+          {/* <video src='https://www.youtube.com/watch?v=ib43NVgp_x0'></video> */}
+          <video loop ref={videoRef} src={review}></video>
+          <AiFillPlayCircle
+            className="home__reviewVideoIcon"
+            onClick={onVideoPress}
+          />
+          {/* <span className='home__reviewVideoIcon'><AiFillPlayCircle /></span> */}
         </div>
+        <Slide bottom>
+          <div className="home__reviewMessage">
+            <FaQuoteLeft color="#0044FF" className="home__reviewQuote" />
+            <p className="home__reviewText">
+              We are very happy to work with Altar.io. The collaboration goes
+              far beyond a standard IT service: with Altar.io we have a sparring
+              partner that challenges us in our product decisions and is truly
+              passionate about the results developed. The level of
+              professionalism and dedication is exceptional in this industry and
+              we are eager to strengthen our collaboration in the future.
+            </p>
+            <span className="home__reviewer">
+              Philip Schoch - Apiax, Co-Founder
+            </span>
+            <img src={reviewLogo} alt="" className="home__reviewClient" />
+          </div>
+        </Slide>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
