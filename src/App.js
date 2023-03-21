cbe28c826b8423d34e07ae009b89cf3be12d8a48
MeryAmun <chebesiawah@gmail.com>
2023-03-21 14:34:24 +0100

    chat bot icon

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import { Footer, Navbar } from "./components";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages";
-import { IoIosArrowDropupCircle } from 'react-icons/io'
-
+import { IoIosArrowDropupCircle } from 'react-icons/io';
+import {TbMessageChatbot} from 'react-icons/tb'
 function App() {
   return (
     <div className="app">
@@ -12,6 +12,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
       </Routes>
       <Footer />
+<TbMessageChatbot className="app__chatBotIcon"/>
       <a href="#top">
   <IoIosArrowDropupCircle className="app__chatIcon" />
   </a>
